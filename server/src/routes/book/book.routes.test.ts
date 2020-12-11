e3660b07a3bb7e9cf9c8abb954d9c14f964fc3f2
Felipe Rocha <felipemota.rocha@gmail.com>
2020-12-10 23:48:46 -0300

    feat [server]: validation in the book creation, tests adopted to that pattern

--- server/src/routes/book/book.routes.test.ts
+++ server/src/routes/book/book.routes.test.ts
@@ -2,11 +2,17 @@ import request from "supertest";
 
 import app from "../../app";
 
-import { MOCKED_BOOK } from "../../utils/tests.utils";
+import {
+	MOCKED_AUTHOR,
+	MOCKED_BOOK,
+	MOCKED_GENRE,
+} from "../../utils/tests.utils";
 
 describe("Books Related Requests", () => {
 	describe("Create a book", () => {
 		it("should create a book", async () => {
+			await request(app).post("/authors").send(MOCKED_AUTHOR).expect(201);
+			await request(app).post("/genres").send(MOCKED_GENRE).expect(201);
 			await request(app).post("/books").send(MOCKED_BOOK).expect(201);
 		});
 	});
--- server/src/routes/book/book.routes.ts
+++ server/src/routes/book/book.routes.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router } from "express";
+import Author from "../../models/author/author.model";
 import Book from "../../models/book/book.model";
+import Genre from "../../models/genre/genre.model";
 
 const router = Router();
 
@@ -14,6 +16,18 @@ router.get("/", async (_req: Request, res: Response) => {
 
 router.post("/", async (req: Request, res: Response) => {
 	try {
+		const {
+			body: { author, genre },
+		} = req;
+
+		const authorExists = await Author.findById(author);
+		if (!authorExists)
+			throw new Error("Please enter a valid author for the book.");
+
+		const genreExists = await Genre.findById(genre);
+		if (!genreExists)
+			throw new Error("Please enter a valid genre for the book.");
+
 		const createdBook = new Book(req.body);
 		await createdBook.save();
 
