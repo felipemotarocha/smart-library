91b64af37af8be12f25c6376133c08b64f1c7098
Felipe Rocha <felipemota.rocha@gmail.com>
2020-12-13 15:57:40 -0300

    [client]: initial commit

---
+++ client/src/App/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+import { GlobalStyle } from "./App.styles";
+
+const App = () => {
+	return (
+		<>
+			<GlobalStyle />
+			<h1>hello world!</h1>
+		</>
+	);
+};
+
+export default App;
