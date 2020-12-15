95d4ebd3336db04b2c9c4101d526f9855aa3c9f3
Felipe Rocha <felipemota.rocha@gmail.com>
2020-12-14 22:20:04 -0300

    feat [client]: styling improvements and view details button

--- client/src/components/book-item/book-item.component.tsx
+++ client/src/components/book-item/book-item.component.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import * as React from "react";
 
 import { Book } from "src/types/book.types";
@@ -11,6 +12,9 @@ const BookItem: React.FunctionComponent<BookItemProps> = ({ book }) => {
 	return (
 		<BookItemContainer>
 			<CoverImage src={book.coverImageUrl} />
+			<Button variant="contained" color="primary">
+				View Details
+			</Button>
 		</BookItemContainer>
 	);
 };
--- client/src/components/book-item/book-item.styles.tsx
+++ client/src/components/book-item/book-item.styles.tsx
@@ -10,9 +10,26 @@ export const BookItemContainer = styled.div`
 	position: relative;
 	box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 	border-radius: 5px;
+	display: flex;
+	align-items: flex-end;
+	justify-content: center;
 
 	&:nth-child(even) {
 		margin: 0 1.5rem;
+	}
+
+	button {
+		margin: 1rem 0;
+		width: 80%;
+		opacity: 0%;
+		transition: all 0.5s ease;
+	}
+
+	&:hover {
+		button {
+			display: initial;
+			opacity: 100%;
+		}
 	}
 `;
 
